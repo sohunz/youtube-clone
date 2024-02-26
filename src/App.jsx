e815a906efadab734aaeaeaa04e958e78dcb51d9
sohunz <sohunily@gmail.com>
2024-02-26 13:45:06 +0700

    configure route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,25 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Video from "./pages/Video/Video";
 import SubscribeContext from "./context/SubscribeContext";
 import CreateVideo from "./components/modal/CreateVideo";
+import Gaming from "./pages/Navbar/Gaming";
+import Help from "./pages/Navbar/Help";
+import History from "./pages/Navbar/History";
+import Music from "./pages/Navbar/Music";
+import ReportHistory from "./pages/Navbar/ReportHistory";
+import SendFeedback from "./pages/Navbar/SendFeedback";
+import Setting from "./pages/Navbar/Setting";
+import Short from "./pages/Navbar/Short";
+import Sport from "./pages/Navbar/Sport";
+import Subscription from "./pages/Navbar/Subscription";
+import Trending from "./pages/Navbar/Trending";
+import WatchLater from "./pages/Navbar/WatchLater";
+import YourChannel from "./pages/Navbar/YourChannel";
+import YourClip from "./pages/Navbar/YourClip";
+import YourVideo from "./pages/Navbar/YourVideo";
+import YoutubeKids from "./pages/Navbar/YoutubeKids";
+import YoutubePremium from "./pages/Navbar/YoutubePremium";
+import YoutubeStudio from "./pages/Navbar/YoutubeStudio";
+import YoutubeMusic from "./pages/Navbar/YoutubeMusic";
 
 const App = () => {
     const [sidebar, setSidebar] = useState(false);
@@ -44,6 +63,31 @@ const App = () => {
                             element={<Channel />}
                         />
                         <Route path="/upload" element={<CreateVideo />} />
+                        <Route path="/short" element={<Short />} />
+                        <Route path="/gaming" element={<Gaming />} />
+                        <Route path="/help" element={<Help />} />
+                        <Route path="/history" element={<History />} />
+                        <Route path="/music" element={<Music />} />
+                        <Route path="/report" element={<ReportHistory />} />
+                        <Route path="/feedback" element={<SendFeedback />} />
+                        <Route path="/setting" element={<Setting />} />
+                        <Route path="/sport" element={<Sport />} />
+                        <Route
+                            path="/subscription"
+                            element={<Subscription />}
+                        />
+                        <Route path="/trending" element={<Trending />} />
+                        <Route path="/watch-later" element={<WatchLater />} />
+                        <Route path="/channel" element={<YourChannel />} />
+                        <Route path="/clip" element={<YourClip />} />
+                        <Route path="/video" element={<YourVideo />} />
+                        <Route path="/kids" element={<YoutubeKids />} />
+                        <Route path="/premium" element={<YoutubePremium />} />
+                        <Route
+                            path="/youtube-music"
+                            element={<YoutubeMusic />}
+                        />
+                        <Route path="/studio" element={<YoutubeStudio />} />
                     </Routes>
                 </SubscribeContext.Provider>
             </DataContext.Provider>
--- src/pages/Navbar/Subscripts.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Subscripts = () => {
-    return <div>Subscripts</div>;
-};
-
-export default Subscripts;
