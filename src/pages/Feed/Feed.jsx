4ff0904a9c0e294125a319746fb532b38f405019
sohunz <sohunily@gmail.com>
2024-03-01 07:19:03 +0700

    responsive done

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -7,7 +7,6 @@ const Feed = () => {
         <div className="w-auto mr-5 h-screen mt-36 lg:ml-20 md:ml-20 sm:ml-5 ml-5 z-0">
             <div className="max-w-[1550px] mx-auto grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 pb-10">
                 <Category />
-                <p>Hello</p>
                 <FeedCard />
             </div>
         </div>
